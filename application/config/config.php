b473088a3e7b74414f7978a4ced34c26774bc21a
Mehtab645 <mehtab.makent@gmail.com>
2024-11-07 17:26:23 +0530

    git

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/cricket/';
+$config['base_url'] = 'https://next-cricket.webtesting.pw/';
 $config['display_errors'] = FALSE;
 
 /*
